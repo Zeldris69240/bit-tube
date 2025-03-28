420a0ef7e07aa8f6f23bc83445542348374846e7
FraGGy123 <richardheisnam@gmail.com>
2025-03-28 20:09:59 +0530

    updated

--- src/app/feed/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return ( 
-        <div>
-            Feed Page!
-        </div>
-     );
-}
- 
-export default Page;
---
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -0,0 +1,27 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Homenavebar } from "../components/home-navbar";
+import { HomeSidebar } from "../components/home-sidebar";
+
+
+interface HomeLayoutProps {
+    children: React.ReactNode;
+};
+
+export const HomeLayout =({ children }: HomeLayoutProps) => {
+    return (
+       
+        <SidebarProvider>
+             <div className="w-full">
+             <Homenavebar />
+               <div className="flex min-h-screen pt-[4rem]">
+                 <HomeSidebar />
+                 <main className="flex-1 overflow-y-auto">
+                  {children}
+                  </main>
+               </div>
+            </div>
+        </SidebarProvider>
+    );
+};
+
+
