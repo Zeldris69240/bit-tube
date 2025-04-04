f5ec197254bcdc562e664630d37898dafcd5570d
ZelDriS <128058092+Zeldris69240@users.noreply.github.com>
2025-04-05 03:11:52 +0530

    Refactor: updated studio components and added infinite scroll hook

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -3,5 +3,5 @@ import { Ratelimit } from '@upstash/ratelimit';
 
 export const ratelimit = new Ratelimit({
     redis,
-    limiter: Ratelimit.slidingWindow(10, '10 s'),
+    limiter: Ratelimit.slidingWindow(100, '10 s'),
 })
--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button"
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react"
@@ -18,9 +19,19 @@ export const StudioUploadModal = () => {
     });
 
     return (
+        <>
+        <ResponsiveModal
+            title="Upload Video"
+            open={!!create.data}
+            onOpenChange={() => create.reset()}
+        >
+            <p>This will be an uploader</p>
+
+        </ResponsiveModal>
         <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
             {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
             Create
         </Button>
+        </>
     );
 };
